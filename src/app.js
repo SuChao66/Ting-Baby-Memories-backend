685fcb3467066460ef87e35da9899ee2e57e03f2
SuChao66 <suchao12138@163.com>
2026-08-10 19:22:16 +0800

    feat: 增加异步捕获机制

--- src/app.js
+++ src/app.js
@@ -1,15 +1,18 @@
 // 引入 express 模块
 const express = require("express");
 // 引入注册中间件模块
-const registerAllMiddleware = require("./global/index");
+const {
+  registerPreMiddleware,
+  registerPostMiddleware,
+} = require("./global/index");
 // 引入路由模块
 const routes = require("./routes");
 
 // 创建 express 应用实例
 const app = express();
 
-// 注册第三方中间件
-registerAllMiddleware(app);
+// 注册前置中间件（cors/helmet/rateLimit/morgan）
+registerPreMiddleware(app);
 // parse requests with a Content-Type of application/json
 app.use(express.json());
 // parse requests with a Content-Type of application/x-www-form-urlencoded
@@ -17,5 +20,8 @@ app.use(express.urlencoded({ extended: true }));
 
 // 注册路由
 app.use("/api", routes);
+
+// 注册后置中间件（404 + 错误处理，必须在路由之后）
+registerPostMiddleware(app);
 
 module.exports = app;
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,7 +1,11 @@
 const Response = require("./util/response");
 const JWT = require("./util/jwt");
+const ApiError = require("./util/ApiError");
+const catchAsync = require("./util/catchAsync");
 
 module.exports = {
   Response,
   JWT,
+  ApiError,
+  catchAsync,
 };
