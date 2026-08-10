45e49ab5d065e4c3b061ec608337db79e36fdf23
SuChao66 <suchao12138@163.com>
2026-08-10 20:00:51 +0800

    feat: 新增Winston日志系统

--- src/utils/index.js
+++ src/utils/index.js
@@ -2,10 +2,12 @@ const Response = require("./util/response");
 const JWT = require("./util/jwt");
 const ApiError = require("./util/ApiError");
 const catchAsync = require("./util/catchAsync");
+const logger = require("./util/logger");
 
 module.exports = {
   Response,
   JWT,
   ApiError,
   catchAsync,
+  logger,
 };
